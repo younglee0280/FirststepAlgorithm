53d06be78c317b110bcc77b440b0009c05d48a97
younglee0280 <younglee0280@gmail.com>
2023-04-08 21:29:21 +0900

    second

--- Java_Algorithm/src/multiplicationtable/MultiplicationTableTrace.java
+++ Java_Algorithm/src/multiplicationtable/MultiplicationTableTrace.java
@@ -8,9 +8,9 @@ public class MultiplicationTableTrace {
 		for (step = 1; step <= 9; step++) {
 			System.out.printf("%d´Ü", step);
 			for (num = 1; num <= 9; num++) {
-				System.out.printf("\t%2d", step * num);
-				// System.out.printf("step = %d, num = %d, step * num = %d\n", step, num, step *
-				// num);
+				//System.out.printf("\t%2d", step * num);
+				System.out.printf("step = %d, num = %d, step * num = %d\n", step, num, step *
+				num);
 			}
 			System.out.printf("\n");
 		}
