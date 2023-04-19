a06135214e99729bdea51d743306a64e210c4a24
younglee0280 <younglee0280@gmail.com>
2023-04-19 22:25:52 +0900

    sixth - 3

--- Java_Algorithm/src/multiplicationtable/MultiplicationSum_11022_BAEK.java
+++ Java_Algorithm/src/multiplicationtable/MultiplicationSum_11022_BAEK.java
@@ -2,28 +2,30 @@ package multiplicationtable;
 
 import java.util.Scanner;
 
-public class MultiplicationSum_11022_Array {
+public class MultiplicationSum_11022_BAEK {
 	
+//	public class Main{
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		
-		int i;
+		//int i;
 		int n = scn.nextInt();
 		int a[] = new int[n];
 		int b[] = new int[n];
 		int array[] = new int[n];
 		
-		for(i = 0; i < a.length; i++) {
+		for(int i = 0; i < n; i++) {
 			a[i] = scn.nextInt();
 			b[i] = scn.nextInt();
 			array[i] = a[i] + b[i];
 		}
 		
-		for(i=0; i< n; i++) {
-			System.out.print("Case #");
-			System.out.print((i+1)+": ");
+		for(int i=0; i< n; i++) {
+			//System.out.print("Case #");
+			//System.out.print((i+1)+": ");
 			System.out.println(a[i]+" + "+b[i]+" = "+array[i]);
 		}
 
 	}
 }
+//}
