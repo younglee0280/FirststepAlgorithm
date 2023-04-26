975fbd1de846ba95bd348fabc7dca9dfb00b44fd
younglee0280 <younglee0280@gmail.com>
2023-04-27 00:00:22 +0900

    eight - 3

--- Java_Algorithm/src/binarysearchtree/printLogicalBST.java
+++ Java_Algorithm/src/binarysearchtree/printLogicalBST.java
@@ -3,14 +3,27 @@ package binarysearchtree;
 public class printLogicalBST {
 	public static void printLogicalBST(int currentIdx) {
 		if(currentIdx != -1) {
-			System.out.printf("tree[%d] : data = %d, left = %d, right = %d\n", currentIdx, tree[currentIdx].data,
-					tree[currentIdx].left, tree[currentIdx].right);
+			System.out.printf("tree[%d] : data = %d, left = %d, right = %d\n", currentIdx,
+					tree[currentIdx].data, tree[currentIdx].left, tree[currentIdx].right);
+			
+			printLogicalBST(tree[currentIdx].left);
+			printLogicalBST(tree[currentIdx].right);
 		}
 	}
 	
-	
 	public static void main(String[] args) {
-
+		for(int i = 0; i<tree.length; i++) {
+			tree[i] = new BST();
+		}
+		addBST(4);
+		addBST(6);
+		addBST(5);
+		addBST(2);
+		addBST(3);
+		addBST(7);
+		addBST(1);
+		
+		
 	}
 
 }
