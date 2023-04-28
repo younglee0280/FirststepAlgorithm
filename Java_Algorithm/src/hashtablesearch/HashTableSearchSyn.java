3306cb6baed7f7ded31aade64311c09d02d0378f
younglee0280 <younglee0280@gmail.com>
2023-04-28 23:55:29 +0900

    ninth - 2

--- Java_Algorithm/src/hashtablesearch/HashTableSearchSyn.java
+++ Java_Algorithm/src/hashtablesearch/HashTableSearchSyn.java
@@ -4,44 +4,59 @@ import java.util.Scanner;
 
 public class HashTableSearchSyn {
 
-		public static int[] hashTable = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+	public static int[] hashTable = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
 
-		public static int hashFunc(int data) {
-			return data % 10;
-		}
-		
-		public static void main(String[] args) {
-			int data, hashValue;
-			int pos;
-			
-			Scanner scn = new Scanner(System.in);
-			do {
-				System.out.printf("\n저장할 데이터 = ");
-				data = scn.nextInt();
-				
-				if(data < 0) {
-					break;
-				}
-				
-				hashValue = hashFunc(data);
-				
-				pos = hashValue;
-				while (hashTable[pos] != -1) {
-					pos++;
-				if(pos <= hashTable.length) {
+	public static int hashFunc(int data) {
+		return data % 10;
+	}
+
+	public static void main(String[] args) {
+		int data, hashValue;
+		int pos;
+
+		Scanner scn = new Scanner(System.in);
+		do {
+			System.out.printf("\n저장할 데이터 = ");
+			data = scn.nextInt();
+
+			if (data < 0) {
+				break;
+			}
+
+			hashValue = hashFunc(data);
+
+			pos = hashValue;
+			while (hashTable[pos] != -1) {
+				pos++;
+				if (pos <= hashTable.length) {
 					pos = 0;
 				}
-				
-				if(pos == hashValue) {
+
+				if (pos == hashValue) {
 					break;
 				}
 			}
-				if(hashTable[pos] == -1) {
-					hashTable[pos] = data;
-				}else {
-					System.out.printf("해시 테이블이 가득 찼습니다\n");
-				}
-			}while(true);
+			if (hashTable[pos] == -1) {
+				hashTable[pos] = data;
+			} else {
+				System.out.printf("해시 테이블이 가득 찼습니다\n");
+			}
+		} while (true);
+		
+		do {
+			System.out.printf("\n검색할 데이터 = ");
+			data = scn.nextInt();
+			
+			if(data < 0) {
+				break;
+			}
+			hashValue = hashFunc(data);
+			
+			pos = hashValue;
+			
+			
+		}while(true);
+		scn.close();
 	}
 
 }
