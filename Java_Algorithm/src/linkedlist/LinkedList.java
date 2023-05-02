bc4d0f5d65f9898e4a33317a2b360b830713a2bf
younglee0280 <younglee0280@gmail.com>
2023-05-03 02:01:57 +0900

    eleventh

--- Java_Algorithm/src/linkedlist/LinkedList.java
+++ Java_Algorithm/src/linkedlist/LinkedList.java
@@ -1,7 +1,6 @@
 package linkedlist;
 
 public class LinkedList {
-	//연결 리스트의 실체가 되는 배열(요소 수는 최대 10으로 한다)
 	public static StationList[] list = new StationList[10];
 
 	public static int head;
@@ -11,7 +10,6 @@ public class LinkedList {
 			list[i] = new StationList();
 		}
 		
-		//역 이름과 연결 정보 설정
 		list[0].name = "부산";
 		list[0].next = -1;
 		list[1].name = "대전";
@@ -23,10 +21,8 @@ public class LinkedList {
 		list[4].name = "아산";
 		list[4].next = 1;
 		
-		//head 값 설정
 		head = 2;
 	}
-		//연결 리스트의 요소를 표시하는 메서드
 		public static void printStationList() {
 			int idx = head;
 			while (idx != -1) {
@@ -37,20 +33,17 @@ public class LinkedList {
 			System.out.printf("\n");
 		}
 		
-		////연결 리스트에 요소를 삽입하는 메서드
 		public static void insertStationList(int insIdx, String insName, int prevIdx) {
 			list[insIdx].name = insName;
 			list[insIdx].next = list[prevIdx].next;
 			list[prevIdx].next = insIdx;
 		}
 		
-		//프로그램 실행의 시작점인 main 메서드
 		public static void main(String[] args) {
 			//초기 상태의 연결 리스트를 작성해 표시
 			initStationList();
 			printStationList();
 	
-		////연결 리스트에 요소를 삽입하여 표시
 			insertStationList(5, "광명", 2);
 			printStationList();
 		}
