7af4b47d4475c9ed9f2dc4d6b8e346bc49d44496
younglee0280 <younglee0280@gmail.com>
2023-05-03 16:09:44 +0900

    twelfth

--- Java_Algorithm/src/quicksort/QuickSort.java
+++ Java_Algorithm/src/quicksort/QuickSort.java
@@ -1,7 +1,29 @@
 package quicksort;
 
 public class QuickSort {
-
+	public static void printArray(int[] a) {
+	
+	for(int i = 0; i < a.length; i++) {
+		System.out.printf("[" + a[i] + "]");
+	}
+	System.out.printf("\n");
+	}
+	
+	public static int divideArray(int[] a, int head, int tail) {
+		int left, right, temp;
+		left = head + 1;
+		right = tail;
+		
+		while(true) {
+			while(left < tail && a[head] > a[left]) {
+				left++;
+			}
+		while(a[head] < a[right]) {
+			right--;
+		}
+		}
+		
+	}
 	public static void main(String[] args) {
 
 	}
