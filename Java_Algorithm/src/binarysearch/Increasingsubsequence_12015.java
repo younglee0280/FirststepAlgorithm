ae55aaab0bbc27e4223b8e046435e811c8e0249c
younglee0280 <younglee0280@gmail.com>
2023-05-05 00:05:52 +0900

    13th

---
+++ Java_Algorithm/src/binarysearch/Increasingsubsequence_12015.java
@@ -0,0 +1,9 @@
+package binarysearch;
+
+public class Increasingsubsequence_12015 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- Java_Algorithm/src/binarysearch/binarysearch_1920.java
+++ Java_Algorithm/src/binarysearch/binarysearch_1920.java
@@ -1,6 +1,6 @@
 package binarysearch;
 
-public class binary {
+public class binarysearch_1920 {
 
 	public static void main(String[] args) {
 
