63d50838b280a17c00a40886f329536f10edfa74
younglee0280 <younglee0280@gmail.com>
2023-05-07 00:00:06 +0900

    fourteenth - 3

--- Java_Algorithm/src/dp/FibonnaciRecDP.java
+++ Java_Algorithm/src/dp/FibonnaciRecDP.java
@@ -1,35 +1,33 @@
 package dp;
 
 public class FibonnaciRecDP {
-	
+
 	public static int[] fibonacciNumbers = new int[100];
-	
 
 	public static void initFibonacciNumbers() {
 		for (int i = 0; i < fibonacciNumbers.length; i++) {
 			fibonacciNumbers[i] = -1;
 		}
 	}
+
 	public static int fibonacci(int n) {
 		System.out.printf("fibonacci(%d)가 호출");
-	
-		if(fibonacciNumbers[n] == -1) {
-			if(n==0) {
+
+		if (fibonacciNumbers[n] == -1) {
+			if (n == 0) {
 				fibonacciNumbers[n] = 0;
-			}
-			else if(n == 1) {
+			} else if (n == 1) {
 				fibonacciNumbers[n] = 1;
-			}
-			else {
-				fibonacciNumbers[n] = fibonacci(n-1) + fibonacci(n-2);
+			} else {
+				fibonacciNumbers[n] = fibonacci(n - 1) + fibonacci(n - 2);
 			}
 		}
 		return fibonacciNumbers[n];
 	}
-	
+
 	public static void main(String[] args) {
 		initFibonacciNumbers();
-		
+
 		System.out.printf("5번째 피보나치 수 = %d\n", fibonacci(5));
 
 	}
