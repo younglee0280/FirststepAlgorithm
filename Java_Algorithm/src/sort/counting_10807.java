159dcb72cec169f453b95cadfea30b38a1bec783
younglee0280 <younglee0280@gmail.com>
2023-05-12 23:50:20 +0900

    nineteenth

--- Java_Algorithm/src/sort/counting_10807.java
+++ Java_Algorithm/src/sort/counting_10807.java
@@ -1,10 +1,18 @@
 package sort;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class counting_10807 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
 	}
 
 }
