6638a15031dbf808a70952ae78b1c69a0efcefa7
younglee0280 <younglee0280@gmail.com>
2023-05-28 23:50:08 +0900

    2023/05/28

--- Java_Algorithm/src/bubblesort/BubbleSortToStringMethod.java
+++ Java_Algorithm/src/bubblesort/BubbleSortToStringMethod.java
@@ -7,6 +7,9 @@ public class BubbleSortToStringMethod {
 		 * 그냥 배열을 출력하면 원하는 형태로 출력되는 걸 볼 수 없음,
 		 * java.util.Arrays의 toString 메서드를 사용하여 배열을 출력
 		 */
+		int a[] = {90, 34, 78, 12, 56};
+		
+		int temp = 0;
 	}
 
 }
