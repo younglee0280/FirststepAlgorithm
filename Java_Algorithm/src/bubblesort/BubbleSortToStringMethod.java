ac6427374cfb6ac56d0aca4c9fbee7187471e970
younglee0280 <younglee0280@gmail.com>
2023-05-29 23:59:26 +0900

    2023/05/29

--- Java_Algorithm/src/bubblesort/BubbleSortToStringMethod.java
+++ Java_Algorithm/src/bubblesort/BubbleSortToStringMethod.java
@@ -2,29 +2,23 @@ package bubblesort;
 
 public class BubbleSortToStringMethod {
 
-	//Type mismatch: cannot convert from long to
-	public long solution(long n) {
+	public static void main() {
+		int[] array = {6, 4, 2, 1, 3, 5};
 		
-		char a[] = String.valueOf(n).toCharArray();
+		int temp = 0;
 		
-		char temp = 'a';
-		for(int i = a.length - 1; i > -1; i--) {
-			for(int j = 0; j < i; j++) {
-				if(a[j] < a[j + 1]) {
-					temp = a[j];
-					a[j] = a[j+1];
-					a[j+1] = temp;
+		for(int i = array.length - 1; i > -1; i--) {
+			for(int j = 0; j < 1; j++) {
+				if(array[j] > array[j+1]) {
+					temp = array[j];
+					array[j] = array[j +1];
+					array[j+1] = temp;
 				}
 			}
 		}
-		
-		String str = "";
-		for(char i : a) {
-			str += i;
-		}
-		
-		return Long.parseLong(str);
 	}
+	
+	//	System.out.println(Arrays.toString(array));
 	/*
 	 * 그냥 배열을 출력하면 원하는 형태로 출력되는 걸 볼 수 없음,
 	 * java.util.Arrays의 toString 메서드를 사용하여 배열을 출력
