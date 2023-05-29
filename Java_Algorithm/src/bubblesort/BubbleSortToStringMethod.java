166c30ae89704cb971fa8029a4618a8c75645c5c
younglee0280 <younglee0280@gmail.com>
2023-05-30 00:00:46 +0900

    2023/05/29

--- Java_Algorithm/src/bubblesort/BubbleSortToStringMethod.java
+++ Java_Algorithm/src/bubblesort/BubbleSortToStringMethod.java
@@ -3,16 +3,16 @@ package bubblesort;
 public class BubbleSortToStringMethod {
 
 	public static void main() {
-		int[] array = {6, 4, 2, 1, 3, 5};
+		int a[] = {6, 4, 2, 1, 3, 5};
 		
 		int temp = 0;
 		
-		for(int i = array.length - 1; i > -1; i--) {
+		for(int i = a.length - 1; i > -1; i--) {
 			for(int j = 0; j < 1; j++) {
-				if(array[j] > array[j+1]) {
-					temp = array[j];
-					array[j] = array[j +1];
-					array[j+1] = temp;
+				if(a[j] > a[j+1]) {
+					temp = a[j];
+					a[j] = a[j +1];
+					a[j+1] = temp;
 				}
 			}
 		}
