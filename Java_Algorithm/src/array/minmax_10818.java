154568a9d6cfb6e53acaf80bf4eb0dc7e071fef5
younglee0280 <younglee0280@gmail.com>
2023-05-31 03:15:47 +0900

    2023/05/30

--- Java_Algorithm/src/array/minmax_10818.java
+++ Java_Algorithm/src/array/minmax_10818.java
@@ -1,13 +1,24 @@
 package array;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class minmax_10818 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		BufferedReader br = new
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+				
 		int N = Integer.parseInt(br.readLine());
 		
+		String s = br.readLine();
+		String a[] = s.split(" " + " " + " " + " " + " ");
+		
+		System.out.println();
 	}
 
 }
