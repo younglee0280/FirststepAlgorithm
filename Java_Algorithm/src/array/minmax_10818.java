24ee2d81644da3f979a1de21c62824a74626e1aa
younglee0280 <younglee0280@gmail.com>
2023-06-02 00:11:35 +0900

    2023/06/01

--- Java_Algorithm/src/array/minmax_10818.java
+++ Java_Algorithm/src/array/minmax_10818.java
@@ -1,11 +1,8 @@
 package array;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class minmax_10818 {
