0ddd619522869a50ffc869372b974e3af7e1a4c2
younglee0280 <younglee0280@gmail.com>
2023-06-22 00:00:30 +0900

    2023/06/21

--- Java_Algorithm/src/array/LessThenX_10871.java
+++ Java_Algorithm/src/array/LessThenX_10871.java
@@ -1,11 +1,17 @@
 package array;
 
+import java.util.Scanner;
+
 public class LessThenX_10871 {
 
 	public static void main(String[] args) {
-/*
- * 
- */
+
+		Scanner scn = new Scanner(System.in);
+		int N, X = scn.nextInt();
+	
+			for(int i = 0; i < N; i++) {
+				System.out.println();
+		}
+	}
 	}
 
-}
