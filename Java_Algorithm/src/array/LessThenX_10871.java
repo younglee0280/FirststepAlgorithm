bdb5e6076846fbdc29ecff6f86593c5661484cef
younglee0280 <younglee0280@gmail.com>
2023-06-22 03:00:28 +0900

    2023/06/22

--- Java_Algorithm/src/array/LessThenX_10871.java
+++ Java_Algorithm/src/array/LessThenX_10871.java
@@ -1,14 +1,23 @@
 package array;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class LessThenX_10871 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner scn = new Scanner(System.in);
-		int N, X = scn.nextInt();
-	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		
 			for(int i = 0; i < N; i++) {
 				System.out.println();
 		}
